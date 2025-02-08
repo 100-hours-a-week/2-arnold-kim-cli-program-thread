6c9c391065016d04530eafe000b10c6e27789192
tpgh02 <seaho4588@gmail.com>
2025-02-08 17:02:23 +0900

    [feat] 음료 주문을 완료해도 다음 음료 주문을 또 받을 수 있도록 기능 추가

--- src/main/java/cafe/view/OutputView.java
+++ src/main/java/cafe/view/OutputView.java
@@ -5,9 +5,8 @@ import main.java.cafe.model.Beverage;
 import java.util.Scanner;
 
 public class OutputView {
-    public void printOrder(Beverage beverage) {
-        String check = "N";
-        while (check.equals("N")) {
+    public String printOrder(Beverage beverage) {
+        while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.println("고객님께서 주문하신 주문 내역입니다.");
             System.out.println();
@@ -15,9 +14,11 @@ public class OutputView {
             System.out.println("가격 : " + beverage.getPrice());
             System.out.println("주문 내역이 맞나요? (Y/N)");
             System.out.print(">> ");
-            check = sc.nextLine().strip();
+            String check = sc.nextLine().strip();
             if (!check.equals("Y") && !check.equals("N")) {
                 System.out.println("Y, N 둘 중 하나만 입력해주세요.");
+            } else {
+                return check;
             }
         }
     }
